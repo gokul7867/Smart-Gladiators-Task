462a0316a2a84eb6af79cdb10779d24ad7fba471
gokul7867 <gokul.s9629@gmail.com>
2026-04-16 19:30:23 +0530

    Initial Commit

---
+++ QuotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuotesApp
+//
+//  Created by gokul gokul on 16/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
